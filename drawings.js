427b18992756f43f26f8ef462b69101102b77ce5
Ellinelliey <hali22ow@student.ju.se>
2023-03-28 15:31:56 +0200

    First row of brick

---
+++ drawings.js
@@ -0,0 +1,29 @@
+/*Foundations of Programming
+Group 28, Linn Halvarsson & Julia Oppmark
+This is a file containing all drawings used for our main game
+*/
+
+x = 100;
+y = 100;
+noStroke();
+fill(218, 255, 239);
+rect(x, y, x + 150, y - 50);
+
+fill(100, 182, 172);
+//First row of bricks
+rect(x, y, 16, 10);
+rect(x + 18, y, 16, 10);
+rect(x + 36, y, 16, 10);
+rect(x + 54, y, 16, 10);
+rect(x + 72, y, 16, 10);
+rect(x + 90, y, 16, 10);
+rect(x + 108, y, 16, 10);
+rect(x + 126, y, 16, 10);
+rect(x + 144, y, 16, 10);
+rect(x + 162, y, 16, 10);
+rect(x + 180, y, 16, 10);
+rect(x + 198, y, 16, 10);
+rect(x + 216, y, 16, 10);
+rect(x + 234, y, 16, 10);
+
+//Second row of bricks
