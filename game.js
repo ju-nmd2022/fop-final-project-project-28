ab8c0247099d8c33708d0292ba4b69be5fbc6b71
opju21 <opju21vj@student.ju.se>
2023-03-31 16:12:36 +0200

    jumpy ig

--- game.js
+++ game.js
@@ -1,4 +1,39 @@
+let yVal = 50;
+let xVal = 50;
+let ySpeed = 0;
+let acc = 0.3;
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(400, 500);
   frameRate(30);
+}
+function draw() {
+  background(16, 17, 24);
+
+  moveVertical();
+  moveHorizontal();
+  rect(xVal, yVal, 20, 50);
+}
+
+function moveVertical() {
+  yVal = yVal + ySpeed;
+
+  if (yVal > 450) {
+    ySpeed = 0;
+  } else {
+    ySpeed = ySpeed + acc;
+  }
+}
+function keyTyped() {
+  if (key === " ") {
+    ySpeed -= 5;
+  }
+}
+
+function moveHorizontal() {
+  if (keyIsDown(68)) {
+    xVal += 2;
+  } else if (keyIsDown(65)) {
+    xVal -= 2;
+  }
 }
