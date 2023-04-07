7366eecc325583407d8eae57f53a4e57ab6f1053
opju21 <opju21vj@student.ju.se>
2023-04-07 13:18:50 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,39 +1,42 @@
-let yVal = 50;
-let xVal = 50;
+let yVal;
+let xVal;
 let ySpeed = 0;
 let acc = 0.3;
+let xSize = 30;
+let ySize = 30;
 
 function setup() {
   createCanvas(400, 500);
   frameRate(30);
+  xVal = 50;
+  yVal = 50;
 }
 function draw() {
   background(16, 17, 24);
+  rect(200, 400, 90, 20);
+  rect(xVal, yVal, xSize, ySize);
 
-  moveVertical();
-  moveHorizontal();
-  rect(xVal, yVal, 20, 50);
-}
-
-function moveVertical() {
   yVal = yVal + ySpeed;
-
-  if (yVal > 450) {
+  moveH();
+  keyPressed();
+  if (yVal > 500 - ySize) {
     ySpeed = 0;
+
+    yVal = 500 - ySize;
   } else {
     ySpeed = ySpeed + acc;
   }
 }
-function keyTyped() {
-  if (key === " ") {
-    ySpeed -= 5;
+
+function keyPressed() {
+  if (key === " " && ySpeed == 0) {
+    ySpeed -= 10;
   }
 }
-
-function moveHorizontal() {
-  if (keyIsDown(68)) {
-    xVal += 2;
-  } else if (keyIsDown(65)) {
-    xVal -= 2;
+function moveH() {
+  if (keyIsDown(65) && xVal > 0) {
+    xVal -= 3;
+  } else if (keyIsDown(68) && xVal < 400 - xSize) {
+    xVal += 3;
   }
 }
