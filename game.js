5eca58cc51129914fdf4d67d235391bf11b12d39
opju21 <opju21vj@student.ju.se>
2023-05-22 19:18:51 +0200

    spikey thingz

--- game.js
+++ game.js
@@ -3,38 +3,30 @@
 // game statges
 let stage = "start";
 let gameTime = 0;
+let diffecultyTime = 0;
+let diffeculty = 1;
 
 //player
 
 //platforms (rectangle)
 let platform = [];
 
-let platformAmount = 3;
+let platformAmount = 7 - diffeculty;
 
 //spikes
 let spike = [];
 let spikeCounter = 0;
+let valueSpike = Math.floor(Math.random() * 5) + 1;
+let spikeCollide = false;
 
 //Canvas /*NEW*/
-let innerHeight = 700; /*NEW*/
+let innerHeight = 600; /*NEW*/
 let innerWidth = 400; /*NEW*/
 
 //Setup
 function setup() {
   createCanvas(innerWidth, innerHeight); /*NEW*/
-  platform1 = new platforms(random(0, innerWidth), -200, random(3, 6), 1);
-  platform2 = new platforms(random(0, 300), -100, random(3, 6), 1);
-  platform3 = new platforms(random(0, 300), 0, random(3, 6), 1);
-  platform4 = new platforms(random(0, 300), 100, random(3, 6), 1);
-  platform5 = new platforms(random(0, 300), 200, random(3, 6), 1); ///// i regret doing it this way but all the 300 is gonna be innerwitdh - platform.width D:
-  for (let i = 0; i < 7; i++) {
-    platform[i] = new platforms(
-      random(0, innerWidth),
-      100 * i,
-      random(3, 6),
-      1
-    );
-  }
+
   player1 = new player(200, 450);
 }
 //end of setup
@@ -44,90 +36,117 @@ function draw() {
   /// call on functions
   if (stage == "game") {
     gameScreen();
-
-    gameTime += 1;
-    spikeCounter += 1; // increment spike counter
-    platformCounter += 1;
   } else if (stage == "start") {
-    yVal = 460;
-    xVal = 200;
     startScreen();
-    gameTime = 0;
-    spikeCounter = 0;
-    platformCounter = 0;
+  } else if (stage == "lose") {
+    loseScreen();
   }
 }
 //end of draw
 
 function startScreen() {
+  // The look of the background
   background(67, 38, 100);
-}
-
-//game
-function gameScreen() {
-  background(16, 17, 24);
-
-  player1.move();
-  player1.display();
-
-  if (stage == "game") {
-    /*for (let i = 0; i < platformAmount; i++) {
-      platform[i] = new platforms(random(0, 300), random(0, 10));
-    }*/
-
-    if (spikeCounter == 150) {
-      spikeCounter = 0; // reset counter
-      let valueSpike = Math.floor(Math.random() * 5) + 1;
-
-      for (let i = 0; i < valueSpike; i++) {
-        spike[i] = new spikes(random(0, 400), 0); ////// 400= innerwidth
-      }
-    }
-
-    // update and display spikes
-    for (let i = 0; i < 5; i++) {
-      spike[i].move();
-      spike[i].display();
-    }
-
-    //update and display platform
-    /* for (let i = 0; i < platformAmount; i++) {
-      platform[i].move();
-      platform[i].display();
-    }*/
-
-    platform1.move();
-    platform1.display();
-    platform2.move();
-    platform2.display();
-    platform3.move();
-    platform3.display();
-    platform4.move();
-    platform4.display();
-    platform5.move();
-    platform5.display();
-  }
-
   //Text
   textSize(10);
   fill(255, 255, 255);
   text(
-    "moveRight =  " +
-      player1.mR +
-      "     moveLeft = " +
-      player1.mL +
-      "     jump = " +
-      player1.j +
-      "     gametime  " +
+    "     time til next lvl: " +
+      diffecultyTime +
+      "     level: " +
+      diffeculty +
+      "     gametime: " +
       gameTime,
     10,
     10,
     width / 2,
     height / 2
   );
+  /// some values that get restarted when u go into the startscreen
+  gameTime = 0;
+  spikeCounter = 0;
+  platformCounter = 0;
+  diffeculty = 1;
+  diffecultyTime = 0;
+  player1.x = 150;
+  player1.y = 500;
+  spikeCollide = false;
+}
+
+function loseScreen() {
+  background(50, 50, 50);
+  //Text
+  textSize(10);
+  fill(255, 255, 255);
+  text(
+    "     level: " + diffeculty + "     gametime: " + gameTime,
+    10,
+    10,
+    width / 2,
+    height / 2
+  );
+}
+
+//game
+function gameScreen() {
+  // aestectics
+  background(16, 17, 24);
+  //Text
+  textSize(10);
+  fill(255, 255, 255);
+  text(
+    "     level: " + diffeculty + "     gametime: " + gameTime,
+    10,
+    10,
+    width / 2,
+    height / 2
+  );
+
+  //values that get uppdated, get added by one (these are mostly timers)
+  gameTime += 1;
+  spikeCounter += 1;
+  diffecultyTime += 1;
+
+  // make the playes
+  player1.move();
+  player1.display();
+
+  if (stage == "game") {
+    if (diffecultyTime === 300) {
+      diffecultyTime = 0; // reset counter
+      diffeculty += 1; // add to the difficulty every time
+    }
+
+    if (spikeCounter == 200) {
+      spikeCounter = 0; // reset counter
+      // create spikes
+      for (let i = 0; i < valueSpike + diffeculty; i++) {
+        spike[i] = new spikes(random(0, innerWidth), 0);
+      }
+    }
+
+    // update and display spikes
+    for (let i = 0; i < valueSpike; i++) {
+      spike[i].move();
+      spike[i].display();
+    }
+
+    /////update platforms
+    for (let i = 0; i < platformAmount; i++) {
+      platform[i].move();
+      platform[i].display();
+    }
+
+    // lose "villkor", how to get to the lose screen
+    if (player1.y + player1.height >= innerHeight && gameTime >= 1000) {
+      stage = "lose";
+    } else if (spikeCollide == true) {
+      stage = "lose";
+    }
+  }
 }
 //end of game
-
+////stuff happens when key is presssed
 function keyPressed() {
   if (key == "d") {
     player1.mR = true;
@@ -139,22 +158,33 @@ function keyPressed() {
   if (key == " ") {
     player1.j = true;
     player1.y -= player1.yS;
-    player1.yS = player1.yS - 8; //jump height!!!!
+    player1.yS = player1.yS - 5;
   }
 
   if (key == "p") {
     stage = "start";
-    spike = [];
   }
   if (key == "o") {
     stage = "game";
-
-    for (let i = 0; i < 10; i++) {
-      spike[i] = new spikes(random(0, 400), 0); //// 400 ska bli innnerwidth
+    ///clears the arrays
+    gameTime = 0;
+    diffecultyTime = 0;
+    diffeculty = 1;
+    spike = [];
+    platform = [];
+    //creates the platforms
+    for (let i = 0; i < platformAmount; i++) {
+      platform[i] = new platforms(
+        random(0, innerWidth),
+        random(100, 150) * -i,
+        random(6) + diffeculty,
+        1 + diffeculty
+      );
     }
-    /*for (let i = 0; i < platformAmount; i++) {
-      platform[i] = new platforms(random(0, 300), 0);
-    }*/
+    // creates the spike after the button has ben pressed
+    for (let i = 0; i < valueSpike + diffeculty; i++) {
+      spike[i] = new spikes(random(0, innerWidth), 0); //// 400 ska bli innnerwidth
+    }
   }
 }
 function keyReleased() {
@@ -196,11 +226,13 @@ class player {
     if (this.mL) {
       this.x -= this.xS;
     }
+    if (this.j) {
+      //jump height!!!!
+    }
 
     //hit the bottom/top of the screen
-    if (this.y + this.height >= 500) {
-      ///////ändra till innerHeight
-      this.y = 500 - this.height; /////här också
+    if (this.y + this.height >= innerHeight) {
+      this.y = innerHeight - this.height;
       this.yS = 0;
     }
     if (this.y <= 0) {
@@ -237,8 +269,8 @@ class platforms {
     }
     if (this.y >= innerHeight) {
       /*NEW*/
-      this.y = 0;
-      this.x = random(0, 300);
+      this.y = -random(100, 150);
+      this.x = random(0, innerWidth - this.width);
     }
     if (
       player1.y + player1.height <= this.y + this.height &&
@@ -265,11 +297,19 @@ class spikes {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.width = 10;
-    this.height = 20;
+    this.width = 20;
+    this.height = 50;
   }
   move() {
-    this.y += random(7);
+    this.y += random(7) + diffeculty;
+    if (
+      this.y + this.height >= player1.y &&
+      this.x <= player1.x + player1.width &&
+      this.y <= player1.y &&
+      this.x + this.width >= player1.x
+    ) {
+      background(67, 2, 89);
+    }
   }
   display() {
     fill(255, 255, 255);
